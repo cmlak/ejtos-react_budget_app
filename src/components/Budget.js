701387b247e509cb5b3b83903f64c445214c0c9d
cmlak <94430451+cmlak@users.noreply.github.com>
2023-02-16 17:12:46 +0700

    Update Budget.js

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,14 +1,15 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
+import withCurrency from '../components/withCurrency';
 
-const Budget = () => {
+const Budget = (props) => {
     const { budget } = useContext(AppContext);
 
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <span>Budget: {props.money} {budget}</span>
         </div>
     );
 };
 
-export default Budget;
+export default withCurrency(Budget);
