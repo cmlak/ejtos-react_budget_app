def5033fd95a6d0870b4fbdebc553df53a194f36
cmlak <94430451+cmlak@users.noreply.github.com>
2023-02-20 16:45:56 +0700

    Update BudgetTwo.js

--- src/components/BudgetTwo.js
+++ src/components/BudgetTwo.js
@@ -1,20 +1,29 @@
-import React, { useContext } from 'react';
-import { AppContext } from './AppContext';
 
-const Budget = (props) => {
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
+import withCurrency from '../components/withCurrency';
+
+const BudgetTwo = (props) => {
     const { budget } = useContext(AppContext);
     
     return (
         <div className='alert alert-secondary'>
             {/* <span>Budget: £{budget}</span> */}
-
+                Budget: {props.money}
             <AppContext.Consumer>
                 {props => {
-                  return <input type="number" value={budget} step={10} name="budget" onChange={(e) => props.dispatch({ type: 'SET_BUDGET', payload: e.target.value }) } />
-                }}
+                  return <input 
+                  type="number" 
+                  value={budget} 
+                  step={10} 
+                  name="budget" 
+                  onChange={(e) => props.dispatch({ 
+                      type: 'SET_BUDGET', payload: e.target.value }) } 
+                      />}}
+                
             </AppContext.Consumer>
         </div>
     );
 };
 
-export default Budget;
+export default withCurrency(BudgetTwo);
